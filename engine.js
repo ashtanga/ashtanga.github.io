629e15701547666a0a32be180650602a333d8438
Alex Petrosh <petrosh@users.noreply.github.com>
2017-03-03 16:39:07 +0100

    Enable Jekyll

--- engine.js
+++ engine.js
@@ -1,8 +1,8 @@
 var fileUrl;
 
 if(window.location.hash && window.location.hash.length == 41){
-	fileUrl = "https://rawgit.com/ashtanga/ashtanga.github.io/" + window.location.hash.substr(1) + "/practice.csv";
-	document.querySelector('a.bottom').innerHTML = window.location.hash.substr(1) + "/practice.csv";
+	fileUrl = "https://rawgit.com/ashtanga/ashtanga.github.io/" + window.location.hash.substr(1) + "/_data/practice.csv";
+	document.querySelector('a.bottom').innerHTML = window.location.hash.substr(1) + "/_data/practice.csv";
 	window.location.href = window.location.pathname;
 	init();
 }else chiamato("https://api.github.com/repos/ashtanga/ashtanga.github.io/git/refs/heads/master?time=" + new Date().getTime());
@@ -329,9 +329,9 @@ function chiamato(url){
 
 function risposta(xhr){
   if(xhr) resp = JSON.parse(xhr.responseText); else resp = { object: { sha: 'master' } };
-  fileUrl = "https://rawgit.com/ashtanga/ashtanga.github.io/" + resp.object.sha + "/practice.csv";
+  fileUrl = "https://rawgit.com/ashtanga/ashtanga.github.io/" + resp.object.sha + "/_data/practice.csv";
   // DEBUG GIST FILE
   // fileUrl = "https://cdn.rawgit.com/petrosh/cc819ea69538dbbffdeafe21b08fbf22/raw/e97c2ed1715a18a6930a5e1c6db336dbba2ce6d6/practice.csv";
-  document.querySelector('a.bottom').innerHTML = resp.object.sha + "/practice.csv";
+  document.querySelector('a.bottom').innerHTML = resp.object.sha + "/_data/practice.csv";
   init();
 }
